f55f7980b68e90af611cd5a0a249b8e51d966acd
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-09-26 01:38:23 -0700

    created the UI for the calculator

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .set('public', path.join(__dirname, 'public'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
